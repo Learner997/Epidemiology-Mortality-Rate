b59e191b4dd6077a4c37988ab1d3bcbc8f7bce63
Xingjian Zhang <xingjianzhang@Xingjians-MacBook-Air.local>
2024-04-26 18:49:23 -0400

    3.0 change

--- R/lifetable-copy.R
+++ R/lifetable-copy.R
@@ -12,12 +12,6 @@
 #'@example example.R
 #'@export
 
-ensure_package <- function(pkg) {
-  if (!require(pkg, character.only = TRUE)) {
-    install.packages(pkg)
-    library(pkg, character.only = TRUE)
-  }
-}
 
 lifetable<-function(x=NULL,n=NULL,wx=NULL,wt=NULL,grp=NULL,dx=NULL,dt=NULL,data=NULL){
 
